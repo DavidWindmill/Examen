c689692f88efe529bf9519c27742230ddff2dc1f
DavidWindmill <125256940+DavidWindmill@users.noreply.github.com>
2025-12-12 10:31:46 +0100

    arreglo rutas

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.staticfiles import StaticFiles
+
 from app.firebase_admin import verify_id_token
+from app.routes.rutas_frontend import router as rutas_frontend
 
 app = FastAPI()
+
+# Static (para /static/...)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Frontend (ruta /)
+app.include_router(rutas_frontend)
+
 bearer = HTTPBearer()
 
 def current_user(creds: HTTPAuthorizationCredentials = Depends(bearer)):
