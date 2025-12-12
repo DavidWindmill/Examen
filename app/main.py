7360d1e8d94c1b6584ea17e90db3e9e48961e5c6
DavidWindmill <125256940+DavidWindmill@users.noreply.github.com>
2025-12-12 09:27:47 +0100

    Arreglo nuevo

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from pathlib import Path
+import sys
+
+BASE_DIR = Path(__file__).resolve().parent
+sys.path.insert(0, str(BASE_DIR))  # <- añade /app al path
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from app.routes.rutas_frontend import router as rutas_frontend
-
-BASE_DIR = Path(__file__).resolve().parent
+from routes.rutas_frontend import router as rutas_frontend
 
 app = FastAPI(title="Kalendas API Gateway", version="1.0.0")
 
@@ -13,8 +16,3 @@ app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="stat
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 app.include_router(rutas_frontend)
-
-
-
-
-
--- app/routes/rutas_frontend.py
+++ app/routes/rutas_frontend.py
@@ -3,12 +3,11 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-router = APIRouter(tags=["Frontend"])
+router = APIRouter()
 
-BASE_DIR = Path(__file__).resolve().parent.parent  # vuelve a /app
+BASE_DIR = Path(__file__).resolve().parent.parent
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 @router.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse("mapa.html", {"request": request})
-
