ee59ff5dab380e1c5da8ff2eb283a4702268cc6c
DavidWindmill <125256940+DavidWindmill@users.noreply.github.com>
2025-12-12 12:17:25 +0100

    backup

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-from pathlib import Path
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.staticfiles import StaticFiles
@@ -11,15 +8,6 @@ from app.routes.rutas_frontend import router as rutas_frontend
 from app.routes.rutas_locations import router as rutas_locations
 
 app = FastAPI()
-
-BASE_DIR = Path(__file__).resolve().parent      # .../app
-STATIC_DIR = BASE_DIR / "static"               # .../app/static
-
-app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
-
-@app.on_event("startup")
-async def startup():
-    await init_db()
 
 # Static (para /static/...)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
